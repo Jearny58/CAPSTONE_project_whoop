2d2983ef8edd70f130cb0da7ce42df89f7ebef77
Joseph Earnshaw <jearnshaw58@gmail.com>
2018-09-05 14:12:21 -0700

    update

---
+++ R_script_CAPSTONE_project_whoop.R
@@ -0,0 +1,13 @@
+# load gdata package
+library(gdata)
+
+# load tidyverse
+library(tidyverse)
+
+read.x
+
+# create data frame from excel spreadsheet
+df_original <- read.xls("capston_WHOOP_data_14aug_18apr.xlsx", sheet = 1, header = TRUE)
+
+                        
+                
