f7f8fe9d53bc61c979a43c1fd1e75e4c0b0be099
Joseph Earnshaw <jearnshaw58@gmail.com>
2018-10-01 18:31:27 -0700

    update

--- R_script_CAPSTONE_project_whoop.R
+++ R_script_CAPSTONE_project_whoop.R
@@ -14,6 +14,14 @@ summary(whoop_df_original)
 # create new data frame to edit
 whoop_edit <- whoop_df_original
 
+sum(is.na(whoop_edit))
+which(is.na(whoop_edit))
+
+whoop_edit[!complete.cases(whoop_edit), ]
+
+na_whoop_edit = whoop_edit[rowSums(is.na(whoop_edit)) > 0, ]
+View(na_whoop_edit)
+
 # remove empty rows 120 - 179
 whoop_edit <- whoop_edit[-c(120:179), ]
 View(whoop_edit)
@@ -130,6 +138,8 @@ summary(strainReg)
 strainReg2 = lm(strain ~ maxHR + cal, data = whoop_edit4)
 summary(strainReg2)
 
+AIC(strainReg2)
+
 ggplot(whoop_edit4, aes(x = maxHR, y = cal)) + 
   geom_point()
 
@@ -166,8 +176,126 @@ total_daily_strain2(180, 3500)
 # library(caret) --> extremely powerful
 # anti_join()
 
+# attempt 2 at creating training and test set for strain regression
+summary(strainReg2)
+
+whoop_df_regression_test = whoop_edit4
+
+library(sigr)
+wrapFTest(strainReg2)
+
+whoop_df_regression_test$prediction = predict(strainReg2, whoop_df_regression_test)
+
+library(ggplot2)
+
+ggplot(whoop_df_regression_test, aes(x = prediction, y = strain)) +
+  geom_point() +
+  geom_abline(color = "blue")
+
+new_strain = data.frame(maxHR = 180, cal = 3500)
+new_strain$prediction = predict(strainReg2, newdata = new_strain)
+new_strain
+
+total_daily_strain2(180, 3500)
+
+whoop_df_regression_test$residuals = whoop_df_regression_test$strain - whoop_df_regression_test$prediction
+
+ggplot(whoop_df_regression_test, aes(x = prediction, y = residuals)) +
+  geom_pointrange(aes(ymin = 0, ymax = residuals)) +
+  geom_hline(yintercept = 0, linetype = 3) 
+
+names(whoop_df_regression_test)
+library(WVPlots)
+GainCurvePlot(whoop_df_regression_test, "prediction", "strain", "Strain Model")
+
+# RMSE 
+strain_error = whoop_df_regression_test$prediction - whoop_df_regression_test$strain
+strain_error2 = strain_error^2
+(strain_rmse = sqrt(mean(strain_error2)))
+sd(whoop_df_regression_test$strain)
+
+# Calculate R^2
+(strain_rss = sum(strain_error2))
+# difference b/w strain and mean strain
+(strain_toterr = whoop_df_regression_test$strain - mean(whoop_df_regression_test$strain))
+(strain_sstot = sum(strain_toterr^2))
+(strain_r_squared = 1 - (strain_rss/strain_sstot))
+
+library(broom)
+(strain_r_squared_glance = glance(strainReg2)$r.squared)
+
+# use nrow to get # of ros in whoop_df_regression_test data set
+(nrow_whoop_df_regression_test = nrow(whoop_df_regression_test))
+
+# calculate how many rows is 75% of the nrows in the data set
+(target_rows = round(0.75 * nrow_whoop_df_regression_test))
+
+# Create the vector of uniform random variables that is the same as nrow in data set
+(ran_variables <- runif(nrow_whoop_df_regression_test))
+
+
+library(caTools)
+
+
+sample_Whoop = sample.split(whoop_df_regression_test, SplitRatio = 0.75)
+
+# using ran_variables create a training and test set with 75/25 split of data
+train_set_whoop_regression_df = subset(whoop_df_regression_test, sample_Whoop == TRUE)
+test_set_whoop_regression_df = subset(whoop_df_regression_test, sample_Whoop == FALSE)
+View(train_set_whoop_regression_df)
+View(test_set_whoop_regression_df)
+
+
+
+# test to see if nrows in train/test set is close to target nrow
+nrow(train_set_whoop_regression_df)
+nrow(test_set_whoop_regression_df)
+
+# saving formula from orginal strainReg2 
+strainReg2_formula = as.formula(strain ~ maxHR + cal)
+
+strain_model = lm(strainReg2_formula, data = train_set_whoop_regression_df)
+summary(strain_model)
+
+View(train_set_whoop_regression_df)
+
+# Create a cross-validation plan
+library(vtreat)
+
+rmse = function()
+
+cross_validation_data_set = whoop_edit4
+
+splitPlan = kWayCrossValidation(nrow_whoop_df_regression_test, 3, NULL, NULL)
+str(splitPlan)
+
+# number of folds
+k = 3
+
+cross_validation_data_set$pred.cv = 0 
+for(i in 1:k) {
+  split = splitPlan[[i]]
+  model = lm(strainReg2_formula, data = cross_validation_data_set[split$train, ])
+  cross_validation_data_set$pred.cv[split$app] = predict(model, newdata = cross_validation_data_set[split$app, ])
+}
+
+# predict from a full model
+cross_validation_data_set$pred = predict(strainReg2, data = cross_validation_data_set)
+
+# RMSE for full model's predictions
+cv_strain_error = cross_validation_data_set$pred - cross_validation_data_set$strain
+cv_strain_error2 = cv_strain_error^2
+(cv_strain_rmse_full = sqrt(mean(cv_strain_error2)))
+
+
+# RMSE for cross-validation predictions
+cvPredict_strain_error = cross_validation_data_set$pred.cv - cross_validation_data_set$strain
+cvPredict_strain_error2 = cvPredict_strain_error^2
+(cvPredict_strain_rmse = sqrt(mean(cvPredict_strain_error2)))
+
+
 # attempt at creating training and test set for strain regression
-set.seed(1)
+set.seed(75)
 
 n = nrow(whoop_edit4)
 shuffled = whoop_edit4[sample(n), ]
